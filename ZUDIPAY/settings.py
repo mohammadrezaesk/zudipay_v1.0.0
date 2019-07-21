dcb9bb555fbef2b6cc51bb66e3420b138a690e1d
mohammadrezaesk <mohammad.1392.sk@gmail.com>
2019-07-21 15:54:03 +0430

    change static

--- ZUDIPAY/settings.py
+++ ZUDIPAY/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     'static'
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media_root')
