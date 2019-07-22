052daaa3eb76eef829ad00847eaf1127c869391f
mohammadrezaesk <mohammad.1392.sk@gmail.com>
2019-07-22 14:51:51 +0430

    test 2

--- ZUDIPAY/settings.py
+++ ZUDIPAY/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'id17xbu41-m5)807w&4(ye6343qz1e_s5v#gnz85_*vy9hwe$g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['185.173.104.121','localhost']
+ALLOWED_HOSTS = ['185.173.104.121','localhost','zudipay.com']
 # Application definition
 
 INSTALLED_APPS = [
