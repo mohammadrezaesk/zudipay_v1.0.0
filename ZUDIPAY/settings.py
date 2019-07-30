eac5208b0654832848bd94ec54f9f03147eb4ee3
mohammadrezaesk <mohammad.1392.sk@gmail.com>
2019-07-30 16:28:04 +0430

    dff

--- ZUDIPAY/settings.py
+++ ZUDIPAY/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'id17xbu41-m5)807w&4(ye6343qz1e_s5v#gnz85_*vy9hwe$g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['185.173.104.121','localhost','zudipay.com']
 # Application definition
@@ -74,8 +74,14 @@ WSGI_APPLICATION = 'ZUDIPAY.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 if DEBUG:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -86,13 +92,7 @@ if DEBUG:
             'PORT': '',
         }
     }
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
